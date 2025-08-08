c934306e8f3ef0820e5967fc05aeeb5638991bc5
Flow2510 <sendra.florian@gmail.com>
2025-08-08 15:24:18 +0200

    ajout d'une condition pour verifier que last est chargé avant de chargé l'eventcard

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,8 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const {data} = useData()
+  const last = data?.events?.[data.events.length -1];    // selection de la derniere prestation par rapport a la liste
   return <>
     <header>
       <Menu />
@@ -116,13 +117,13 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-        <EventCard
+        {last && <EventCard
           imageSrc={last?.cover}
           title={last?.title}
           date={new Date(last?.date)}
           small
           label="boom"
-        />
+        />}
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
